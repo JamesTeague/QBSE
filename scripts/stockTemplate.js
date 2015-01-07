0583a6d56887ed68dd1af257c8f609fc75c01c79
James Teague <jtteague13@gmail.com>
2015-01-07 11:42:07 -0600

    Handlebars!
    Broke Qbs into tiers visibly. Found new block helper for handlebars to
    help.

--- scripts/stockTemplate.js
+++ scripts/stockTemplate.js
@@ -1,4 +1,26 @@
 var ref = new Firebase("https://qb-stock-exchange.firebaseio.com/");
+Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+    switch (operator) {
+        case '==':
+            return (v1 == v2) ? options.fn(this) : options.inverse(this);
+        case '===':
+            return (v1 === v2) ? options.fn(this) : options.inverse(this);
+        case '<':
+            return (v1 < v2) ? options.fn(this) : options.inverse(this);
+        case '<=':
+            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+        case '>':
+            return (v1 > v2) ? options.fn(this) : options.inverse(this);
+        case '>=':
+            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+        case '&&':
+            return (v1 && v2) ? options.fn(this) : options.inverse(this);
+        case '||':
+            return (v1 || v2) ? options.fn(this) : options.inverse(this);
+        default:
+            return options.inverse(this);
+    }
+});
 var qbList = [];
 var source = $("#stockTemplate").html();
 var template = Handlebars.compile(source);
