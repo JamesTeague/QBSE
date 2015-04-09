d2ade52df8c8b032e281380535a6d93791a53eea
James Teague II <jtteague13@gmail.com>
2015-04-09 16:54:34 -0500

    TESTING

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.static(path.join(__dirname, 'public/html')));
 
 
 app.use('/', routes);
+app.use('/market', routes);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
