fea5ff1275a245e5ca5f196e6753101060231be3
James Teague II <jtteague13@gmail.com>
2015-04-09 18:22:12 -0500

    need help...

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ exports.home = function(req, res){
 	res.sendfile('index.html');
 };
 exports.market = function(req, res) {
-	// console.log(path.join(__dirname+'../public/market.html'))
-	res.sendfile('market.html', {root: '/public'});
+	var cwd = process.cwd();
+    var marketFile = cwd + "/market.html";
+    res.sendfile(marketFile);
 };	
