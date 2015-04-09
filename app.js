f9a2d8f37be768f9915901954b2a6edfb8e547db
James Teague II <jtteague13@gmail.com>
2015-04-09 17:12:56 -0500

    TESTING

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/html')));
 
 
 app.use('/', routes);
