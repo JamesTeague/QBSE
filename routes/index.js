854fd0254f75a370ae8b351bc13d973f1c88e883
James Teague II <jtteague13@gmail.com>
2015-04-09 18:11:29 -0500

    gsd

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ exports.home = function(req, res){
 	res.sendfile('index.html');
 };
 exports.market = function(req, res, path) {
-	console.log(path.join(__dirname+'../public/market.html'))
+	// console.log(path.join(__dirname+'../public/market.html'))
 	res.sendfile(path.join(__dirname+'../public/market.html'));
 };	
