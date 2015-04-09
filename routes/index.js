f3146ddcf942e0967f7ffec83c866ea411849810
James Teague II <jtteague13@gmail.com>
2015-04-09 18:00:21 -0500

    TEST

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,5 @@ exports.home = function(req, res){
 	res.sendfile('index.html');
 };
 exports.market = function(req, res) {
-	res.sendfile('index.html');
+	res.sendfile('../public/market.html');
 };	
