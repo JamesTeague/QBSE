0ef274ddcee1b84566b4e0d4ef18e3aba29fbeed
James Teague II <jtteague13@gmail.com>
2015-04-10 10:21:17 -0500

    fixes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,4 +4,4 @@
  * @author James Teague II jtteague13@gmail.com
  * @since 4/7/2015
  */
-var app = angular.module("MainApp", ["firebase"]);
+var app = angular.module("MainApp", ["firebase", "ngRoute"]);
